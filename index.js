3e83000459cd02ea631ddd1c4337debd08f26b83
Michael Vulpo <214732444+MiVuSec@users.noreply.github.com>
2025-07-09 14:03:52 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ console.log('My code is running');
 
 let toDoList = [];
 
+let section = document.querySelector('section');
+let nodes;
+
 let listAdder = (string) => {
-  toDoList.push(string);
-  console.log(toDoList)
+  section.innerHTML += "<p style='float:left; width: 80%'><input type='checkbox' onchange='handleChange(this)'>" + string + "</input></p>";
 }
 
 let btn = document.querySelector('.button');
 
 btn.addEventListener("click", () => {
   let inputValue = document.getElementById("textBox").value;
-listAdder(inputValue)});
+  listAdder(inputValue)
+});
+
+let p;
+function handleChange(checkbox) {
+  if (checkbox.checked) {
+    p = checkbox.parentNode;
+    p.addEventListener("change", () => { p.style.float = "right"; });
+  } else {
+    p = checkbox.parentNode;
+    p.addEventListener("change", () => { p.style.float = "left"; });
+  }
+}
